e897826c8be0cebe84f7ea61adbadffe0e8a9507
srtg4we5gsetrgwhreyt <srtg4we5gsetrgwhreyt@users.noreply.github.com>
2017-09-22 07:13:38 -0400

    Add files via upload

--- R/server.R
+++ R/server.R
@@ -113,7 +113,7 @@ shinyURL.server = function(session, options) {
   clientData = isolate(reactiveValuesToList(session$clientData))
   
   ## base URL which is not supposed to change
-  baseURL = paste0("http://tools.rotationinvest.com/",
+  baseURL = paste0("http://tools.rotationinvest.com",
                    # clientData$url_hostname,
                    # ## add port number if present
                    # if( (port=clientData$url_port)!="" ) paste0(":", port),
